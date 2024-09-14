9ab7f17aeb5271a459937228c7e4a05dda0743e2
PythonAkoto <yawakoto.python@example.com>
2024-09-14 11:40:34 +0100

    1st phase design

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, url_for
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()                   # environment variables
+
+app = Flask(__name__)           # config Flask app
+CORS(app)                       # extra layer of protection
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
